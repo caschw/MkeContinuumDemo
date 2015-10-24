02097e8edf6b57c70c678a660f2360947348b1e9
Carl Schweitzer <carl@caschw.com>
2015-10-23 23:46:19 -0500

    Check in code and presentation

---
+++ Continuum.LiveTiles/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Continuum.LiveTiles
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // same as (ms-appx:///MyFolder/MyFile.txt)
+            var _Folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            _Folder = await _Folder.GetFolderAsync("Assets");
+
+            // acquire file
+            var _File = await _Folder.GetFileAsync("AdaptiveTile.xml");
+
+            // read content
+            var tileString = await Windows.Storage.FileIO.ReadTextAsync(_File);
+            var tileXml = new XmlDocument();
+            tileXml.LoadXml(tileString);
+
+            var notification = new TileNotification(tileXml);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
+        }
+    }
+}
---
+++ Continuum.Misc/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Continuum.Misc
+{
+    public class MainViewModel
+    {
+        public string Greeting { get { return "Hello Milwaukee Code Camp!"; } }
+
+        public string Time { get { return DateTime.Now.ToString("HH:mm:ss"); } }
+    }
+}
